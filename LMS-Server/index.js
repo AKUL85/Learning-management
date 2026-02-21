deed71343f81138a5d542a88e73f0bc8f2c53de4
Drubo Roy Bishwas <doctorhackan@gmail.com>
2026-02-22 00:40:51 +0600

    changed the port of cors origin

--- LMS-Server/index.js
+++ LMS-Server/index.js
@@ -16,7 +16,7 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.use(cookieParser());
